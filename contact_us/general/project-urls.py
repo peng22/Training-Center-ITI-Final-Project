e30382f2b4e6ffd1c4a14902f4f087006eb190f6
peng22 <32569370+peng22@users.noreply.github.com>
2020-02-05 20:37:29 +0200

    contact_us

---
+++ contact_us/general/project-urls.py
@@ -0,0 +1,1 @@
+path('contact_us/', include('contact_us.urls'))
---
+++ contact_us/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+from django import forms
+
+class ContactForm(forms.Form):
+    name=forms.CharField(required=True)
+    from_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message  = forms.CharField(widget=forms.Textarea, required=True)
