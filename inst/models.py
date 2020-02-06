046a3722620e2f0077775c17e8f1bb5947e0e037
Ahmed Khaled <ahmedk4720@gmail.com>
2020-02-06 13:27:29 +0200

    bug fix

--- inst/models.py
+++ inst/models.py
@@ -6,12 +6,12 @@ from datetime import date
 class Instructor(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    date_of_birth = models.DateField('DOB')
+    date_of_birth = models.DateField('Birthday')
     email = models.EmailField()
-    photo = models.ImageField(upload_to = 'profile' , blank=True    )
+    photo = models.ImageField(upload_to = 'profile' , blank=True)
 
     def __str__(self):
-        return self.first_name
+        return self.first_name + self.last_name
     def age(self):
         return date.today() - self.date_of_birth
 
