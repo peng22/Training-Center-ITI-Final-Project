47fe6e514cc6164d109ad444f15c856c1a19b910
Ahmed Khaled <ahmedk4720@gmail.com>
2020-02-07 11:55:50 +0200

    add contanct us page

---
+++ contact/views.py
@@ -0,0 +1,29 @@
+
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+
+
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail( subject, message, from_email, ['mohamedmido22107@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "contact/contact.html", {'form': form})
+
+def successView(request):
+    return render(request, "contact/success.html")
+
